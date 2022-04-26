63f4f74ded1f12682195e04e5a5ef22f228e203d
33085UbukataKaito <33085@ojs.ac.jp>
2022-04-26 13:53:04 +0900

    FeetConverterクラスを静的クラス化

--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter {
     class Program {
-        FeetConverter MeterToFeet = new FeetConverter();
+       // FeetConverter MeterToFeet = new FeetConverter();
         static void Main(string[] args) {
             if (args.Length >= 1 && args[0] == "-tom") {
                 //フィートからメートルへの対応表を出力
@@ -20,18 +20,18 @@ namespace DistanceConverter {
 
         //メートルからフィートへの対応表を出力
         private static void printMeterToFeetList(int start, int stop) {
-            FeetConverter fc = new FeetConverter();
+           // FeetConverter fc = new FeetConverter();
             for (int meter = 1; meter <= 10; meter++) {
-                double feet = fc.ToMeter(meter);
+                double feet = FeetConverter.ToMeter( meter);
                 Console.WriteLine("{10} m = {1:0.0000} ft", meter, feet);
             }
         }
 
         //フィートからメートルへの対応表を出力
         private static void printFeetToMeterList(int start, int stop) {
-            FeetConverter fc = new FeetConverter();
+            //FeetConverter fc = new FeetConverter();
             for (int feet = 1; feet <= 10; feet++) {
-                double meter = fc.FromMeter(feet);
+                double meter = FeetConverter.FromMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
         } 

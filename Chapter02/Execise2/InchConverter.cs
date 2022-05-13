8d3fd49ea0e2a64406ae752b0163a14a4263ca68
33085UbukataKaito <33085@ojs.ac.jp>
2022-05-13 13:19:37 +0900

    問題2-2

---
+++ Chapter02/Execise2/InchConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execise2 {
+    //フィートとメートルの単位変換クラス
+    public static class InchConverter {
+        private const double ratio = 0.0254;
+        //public static readonly double ratio = 0.3048;←外部からもアクセスさせたい場合
+
+        //メートルからフィートを求める(静的メソッド)
+        public static double FromMeter(double meter) {
+            return meter / ratio;
+        }
+
+        //フィートからメートルを求める(静的メソッド)
+        public static double ToMeter(int inch) {
+            return inch * ratio;
+        }
+
+
+    }
+}
---
+++ Chapter02/Execise2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execise2{
+    class Program {
+        // FeetConverter MeterToFeet = new FeetConverter();
+        static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //フィートからメートルへの対応表を出力
+                printInchToMeterList(1, 10);
+            }
+            else {
+                //メートルからフィートへの対応表を出力
+                printMeterToInchList(1, 10);
+            }
+        }
+
+        //メートルからフィートへの対応表を出力
+        private static void printMeterToInchList(int start, int stop) {
+            // FeetConverter fc = new FeetConverter();
+            for (int meter = 1; meter <= 10; meter++) {
+                double inch = InchConverter.ToMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} in", meter, inch);
+            }
+        }
+
+        //フィートからメートルへの対応表を出力
+        private static void printInchToMeterList(int start, int stop) {
+            //FeetConverter fc = new FeetConverter();
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = InchConverter.FromMeter(inch);
+                Console.WriteLine("{0} in = {1:0.0000} m", inch, meter);
+            }
+        }
+    }
+}
+

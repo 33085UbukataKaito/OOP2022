975a2b8b0fce48c6d37e04b60dc9db88238ecd0c
33085UbukataKaito <33085@ojs.ac.jp>
2022-05-31 13:36:05 +0900

    問題5-3-5

--- Chapter05/Excersice03/Program.cs
+++ Chapter05/Excersice03/Program.cs
@@ -7,6 +7,58 @@ using System.Threading.Tasks;
 namespace Excersice03 {
     class Program {
         static void Main(string[] args) {
+
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Exercise3_1(text);
+            Console.WriteLine("-----");
+
+            Exercise3_2(text);
+            Console.WriteLine("-----");
+
+            Exercise3_3(text);
+            Console.WriteLine("-----");
+
+            Exercise3_4(text);
+            Console.WriteLine("-----");
+
+            Exercise3_5(text);
+        }
+
+        private static void Exercise3_1(string text) {
+            int space = text.Count(c => c == ' ');
+
+            Console.WriteLine("空白数:{0}",space);
+        }
+
+        private static void Exercise3_2(string text) {
+            var replaced = text.Replace("big", "small");
+            Console.WriteLine(replaced);
+        }
+
+        private static void Exercise3_3(string text) {
+            int count = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}",count);
+        }
+
+        private static void Exercise3_4(string text) {
+            text.Split(' ').Where(words => words.Length <= 4)
+                    .ToList().ForEach(Console.WriteLine);
+        }
+
+        private static void Exercise3_5(string text) {
+            var words = text.Split(' ');
+            if(words.Length > 0) {
+                var sb = new StringBuilder(words[0]);
+
+                foreach (var word in words.Skip(1)) {
+                    sb.Append(word);
+                    sb.Append(' ');
+                }
+                var texts = sb.ToString();
+                Console.WriteLine(texts);
+            }
+
         }
     }
 }
--- Chapter05/Exercise02/Program.cs
+++ Chapter05/Exercise02/Program.cs
@@ -8,17 +8,30 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
-            int i;
+            //お手本
+            var line = Console.ReadLine();
+            int num;
+            if(int.TryParse(line,out num)) {
+                Console.WriteLine("{0:#,#}", num);
+            }else {
+                Console.WriteLine("数値文字列ではありません");
+            }
 
-            Console.Write("数列を入力してください:");
-            if(int.TryParse(Console.ReadLine(),out i)) {
-                var s = i.ToString("#,0");
-                Console.WriteLine(s);
-                Console.WriteLine("変換に成功しました");
-            }
-            else {
-                Console.WriteLine("変換に失敗しました");
-            }
+
+
+
+            //自作
+            //int i;
+
+            //Console.Write("数列を入力してください:");
+            //if(int.TryParse(Console.ReadLine(),out i)) {
+            //    var s = i.ToString("#,0");
+            //    Console.WriteLine(s);
+            //    Console.WriteLine("変換に成功しました");
+            //}
+            //else {
+            //    Console.WriteLine("変換に失敗しました");
+            //}
 
 
         }

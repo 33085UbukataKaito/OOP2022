1ca5276a88b4a1363b3b8a0503969011872aa7b7
33085UbukataKaito <33085@ojs.ac.jp>
2022-06-02 14:13:06 +0900

    問題6-1

--- Chapter06/Excercise01/Program.cs
+++ Chapter06/Excercise01/Program.cs
@@ -29,21 +29,36 @@ namespace Excercise01 {
         }
 
         private static void Exercise1_2(int[] numbers) {
-            for(int i = numbers.Length-2; i < numbers.Length; i++) {
-                Console.WriteLine(numbers[i]);
+            foreach(var n in numbers.Skip(numbers.Length - 2)) {
+                Console.WriteLine(n);
             }
+
+
+            //for(int i = numbers.Length-2; i < numbers.Length; i++) {
+            //    Console.WriteLine(numbers[i]);
+            //}
         }
 
         private static void Exercise1_3(int[] numbers) {
-            
+           var result = numbers.Select(n => n.ToString());
+
+           foreach( var str in result){
+                Console.WriteLine(str);
+            }  
         }
 
         private static void Exercise1_4(int[] numbers) {
-            
+            Array.Sort(numbers);
+            foreach (var str in numbers.Take(3)){
+                Console.WriteLine(str);
+            }
         }
 
         private static void Exercise1_5(int[] numbers) {
-           
+            var result = numbers.Distinct().Count(n => n > 10);
+
+            Console.WriteLine(result);
+
         }
     }
 }

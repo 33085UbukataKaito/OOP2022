eb905bbf83e484ed2286ae932b88f4b6c9593ea8
33085UbukataKaito <33085@ojs.ac.jp>
2022-06-07 14:49:04 +0900

    サンプルプログラム

---
+++ FormAppSample/Sample0607/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace Sample0607 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nud1 = new System.Windows.Forms.NumericUpDown();
+            this.nud2 = new System.Windows.Forms.NumericUpDown();
+            this.nudAns = new System.Windows.Forms.NumericUpDown();
+            this.nudMod = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAns)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMod)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(145, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "÷";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(345, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 37);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(541, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "余り";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(259, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 92);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nud1
+            // 
+            this.nud1.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nud1.Location = new System.Drawing.Point(19, 49);
+            this.nud1.Name = "nud1";
+            this.nud1.Size = new System.Drawing.Size(120, 44);
+            this.nud1.TabIndex = 3;
+            // 
+            // nud2
+            // 
+            this.nud2.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nud2.Location = new System.Drawing.Point(219, 47);
+            this.nud2.Name = "nud2";
+            this.nud2.Size = new System.Drawing.Size(120, 44);
+            this.nud2.TabIndex = 3;
+            // 
+            // nudAns
+            // 
+            this.nudAns.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nudAns.Location = new System.Drawing.Point(392, 47);
+            this.nudAns.Name = "nudAns";
+            this.nudAns.Size = new System.Drawing.Size(120, 44);
+            this.nudAns.TabIndex = 3;
+            // 
+            // nudMod
+            // 
+            this.nudMod.Font = new System.Drawing.Font("MS UI Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nudMod.Location = new System.Drawing.Point(642, 47);
+            this.nudMod.Name = "nudMod";
+            this.nudMod.Size = new System.Drawing.Size(120, 44);
+            this.nudMod.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.nudMod);
+            this.Controls.Add(this.nudAns);
+            this.Controls.Add(this.nud2);
+            this.Controls.Add(this.nud1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "計算アプリ";
+            ((System.ComponentModel.ISupportInitialize)(this.nud1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAns)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMod)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown nud1;
+        private System.Windows.Forms.NumericUpDown nud2;
+        private System.Windows.Forms.NumericUpDown nudAns;
+        private System.Windows.Forms.NumericUpDown nudMod;
+    }
+}
+
---
+++ FormAppSample/Sample0607/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0607 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e) {
+
+            if (int.Parse(nud2.Text) != 0) {
+                nudAns.Value = nud1.Value / nud2.Value;
+                nudMod.Value = nud1.Value % nud2.Value;
+
+            }
+            else {
+                MessageBox.Show("0で割り算できません", "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}

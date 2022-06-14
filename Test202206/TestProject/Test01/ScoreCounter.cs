2fdc6cb94b64b9016f3498b7677f7c624f8c27d8
33085UbukataKaito <33085@ojs.ac.jp>
2022-06-14 14:50:39 +0900

    テスト途中経過

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -16,25 +16,38 @@ namespace Test01 {
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-        //科目別の点数を求める
-        
+            //テストデータを読み込み、Studentオブジェクトのリストを返す
+            List<Student> student = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student sale = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                student.Add(sale);
+            }
+            return student;
 
 
 
 
-
-
-            
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
+            //科目別の点数を求める
+            var dict = new Dictionary<string, int>();
+            foreach (Student student in _score) {
+                if (dict.ContainsKey(student.Subject))
+                    dict[student.Subject] += student.Score;
+                else
+                    dict[student.Subject] = student.Score;
+            }
 
-
-
-
-
-            
+            return dict;
         }
+
     }
 }

39acefd8b2e7f8de9f5bf6eb2bfd74d885a52529
33085UbukataKaito <33085@ojs.ac.jp>
2022-10-20 11:58:49 +0900

    vm

---
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGame {
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    
+    public partial class MainWindow : Window {
+
+    int seikai = new Random().Next(1, 25);//正解の番号
+        
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender,RoutedEventArgs e) {
+            
+            Button bt = (Button)sender;
+
+            if(seikai == int.Parse((string)bt.Content)) {
+                textBlock.Text = "あたり";
+                bt.Background = Brushes.Yellow;
+            }else {
+                bt.Background = Brushes.Red;
+                textBlock.Text = int.Parse((string)bt.Content) < seikai
+                    ? "もっと大きな数字です！" : "もっと小さな数字です！";
+
+            }
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e) {
+            textBlock.Text = "GAME START!";
+        }
+    }
+}
--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,14 +20,53 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            seasonComboBox.SelectedIndex = getSeasonIndex();
         }
 
-        private void okButtom_Click(object sender, RoutedEventArgs e) {
+        private int getSeasonIndex() {
+            DateTime dtNow = DateTime.Now;
+            int iMonth = dtNow.Month;
+
+            if (iMonth == 3 || iMonth ==4 || iMonth == 5 ) {
+                return 0;
+            }else if(iMonth == 6 || iMonth == 7 || iMonth == 8) {
+                return 1;
+            }else if(iMonth == 9 || iMonth == 10 || iMonth == 11) {
+                return 2;
+            }else{
+                return 3;
+            }
+        }
+
+
+
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "チェック済み";
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            checkBoxTextBlock.Text = "未チェック";
+        }
+
+        private void radioButtom_Checked(object sender, RoutedEventArgs e) {
 
         }
 
-        private void cancelButtom_Click(object sender, RoutedEventArgs e) {
+        private void seasonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = (string)((ComboBoxItem)seasonComboBox.SelectedItem).Content;
+        }
 
+        private void redRadioButtom_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "赤";
+        }
+
+        private void yellowRadioButtom_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "黄";
+        }
+
+        private void blueRadioButtom_Checked(object sender, RoutedEventArgs e) {
+            colorTextBox.Text = "青";
         }
     }
 }

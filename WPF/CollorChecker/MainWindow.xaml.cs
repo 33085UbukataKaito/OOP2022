f622e803e24156b8a75f6f813fa97cb4ae196492
33085UbukataKaito <33085@ojs.ac.jp>
2022-11-07 15:56:32 +0900

    ColorList 挙動不審

--- WPF/CollorChecker/MainWindow.xaml.cs
+++ WPF/CollorChecker/MainWindow.xaml.cs
@@ -20,20 +20,26 @@ namespace CollorChecker {
     /// </summary>
     
     public partial class MainWindow : Window {
+        List<MyColor> myColors = new List<MyColor>(); 
+        MyColor mycolor = new MyColor();
+
         public MainWindow() {
             InitializeComponent();
             DataContext = GetColorList();
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
-            tarou.Background = new SolidColorBrush(Color.FromRgb((byte)int.Parse(R.Text), (byte)int.Parse(G.Text), (byte)int.Parse(B.Text)));
+            SetColor();
         }
 
         private void SetColor() {
             var r = byte.Parse(R.Text);
             var g = byte.Parse(G.Text);
             var b = byte.Parse(B.Text);
-            
+
+            tarou.Background = new SolidColorBrush(Color.FromRgb(r,g,b));
+
+
         }
 
 
@@ -49,14 +55,35 @@ namespace CollorChecker {
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+            mycolor = (MyColor)((ComboBox)sender).SelectedItem;
             var color = mycolor.Color;
             SliderR.Value = color.R;
             SliderG.Value = color.G;
             SliderB.Value = color.B;
-            tarou.Background = new SolidColorBrush(color);
+            tarou.Background = new SolidColorBrush(Color.FromRgb((byte)SliderR.Value,(byte)SliderG.Value,(byte)SliderB.Value));
 
             
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            Nobita.Items.Add("R =" + R.Text + "G =" + G.Text + "B =" + B.Text);
+            MyColor myColor = new MyColor();
+            var r = byte.Parse(R.Text);
+            var g = byte.Parse(G.Text);
+            var b = byte.Parse(B.Text);
+            myColor.Color = Color.FromRgb(r, g, b);
+            myColors.Add(myColor);
+
+        }
+
+        private void Nobita_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            SliderR.Value = myColors[Nobita.SelectedIndex].Color.R;
+            SliderG.Value = myColors[Nobita.SelectedIndex].Color.G;
+            SliderB.Value = myColors[Nobita.SelectedIndex].Color.B;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            SetColor();
         }
     }
 }

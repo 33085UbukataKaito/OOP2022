0394677f62fb122ff4a16168b07588978b1af858
33085UbukataKaito <33085@ojs.ac.jp>
2022-11-17 15:56:16 +0900

    WetherApp 明日明後日の情報

--- chapter14/WetherApp/Form1.Designer.cs
+++ chapter14/WetherApp/Form1.Designer.cs
@@ -29,23 +29,26 @@ namespace WetherApp {
             this.label1 = new System.Windows.Forms.Label();
             this.cbPrifecture = new System.Windows.Forms.ComboBox();
             this.PrefectureName = new System.Windows.Forms.Label();
-            this.pbWether = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pbWether)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbDay = new System.Windows.Forms.ComboBox();
+            this.btDayGet = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDay = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // tbWetherInfo
             // 
             this.tbWetherInfo.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.tbWetherInfo.Location = new System.Drawing.Point(12, 258);
+            this.tbWetherInfo.Location = new System.Drawing.Point(12, 323);
             this.tbWetherInfo.Multiline = true;
             this.tbWetherInfo.Name = "tbWetherInfo";
-            this.tbWetherInfo.Size = new System.Drawing.Size(776, 180);
+            this.tbWetherInfo.Size = new System.Drawing.Size(776, 115);
             this.tbWetherInfo.TabIndex = 0;
             // 
             // btWetherGet
             // 
             this.btWetherGet.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.btWetherGet.Location = new System.Drawing.Point(198, 99);
+            this.btWetherGet.Location = new System.Drawing.Point(198, 33);
             this.btWetherGet.Name = "btWetherGet";
             this.btWetherGet.Size = new System.Drawing.Size(103, 45);
             this.btWetherGet.TabIndex = 1;
@@ -57,7 +60,7 @@ namespace WetherApp {
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(12, 217);
+            this.label1.Location = new System.Drawing.Point(12, 281);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(129, 29);
             this.label1.TabIndex = 2;
@@ -66,7 +69,7 @@ namespace WetherApp {
             // cbPrifecture
             // 
             this.cbPrifecture.FormattingEnabled = true;
-            this.cbPrifecture.Location = new System.Drawing.Point(7, 112);
+            this.cbPrifecture.Location = new System.Drawing.Point(7, 45);
             this.cbPrifecture.Name = "cbPrifecture";
             this.cbPrifecture.Size = new System.Drawing.Size(121, 20);
             this.cbPrifecture.TabIndex = 3;
@@ -75,36 +78,77 @@ namespace WetherApp {
             // 
             this.PrefectureName.AutoSize = true;
             this.PrefectureName.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.PrefectureName.Location = new System.Drawing.Point(3, 75);
+            this.PrefectureName.Location = new System.Drawing.Point(3, 9);
             this.PrefectureName.Name = "PrefectureName";
             this.PrefectureName.Size = new System.Drawing.Size(307, 21);
             this.PrefectureName.TabIndex = 4;
             this.PrefectureName.Text = "天気を知りたい地方をお選びください";
             // 
-            // pbWether
+            // label2
             // 
-            this.pbWether.Location = new System.Drawing.Point(409, 12);
-            this.pbWether.Name = "pbWether";
-            this.pbWether.Size = new System.Drawing.Size(367, 234);
-            this.pbWether.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pbWether.TabIndex = 5;
-            this.pbWether.TabStop = false;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(3, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(290, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = ".天気を知りたい日をお選びください";
+            // 
+            // cbDay
+            // 
+            this.cbDay.FormattingEnabled = true;
+            this.cbDay.Location = new System.Drawing.Point(7, 142);
+            this.cbDay.Name = "cbDay";
+            this.cbDay.Size = new System.Drawing.Size(121, 20);
+            this.cbDay.TabIndex = 7;
+            // 
+            // btDayGet
+            // 
+            this.btDayGet.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btDayGet.Location = new System.Drawing.Point(198, 125);
+            this.btDayGet.Name = "btDayGet";
+            this.btDayGet.Size = new System.Drawing.Size(103, 45);
+            this.btDayGet.TabIndex = 1;
+            this.btDayGet.Text = "決定";
+            this.btDayGet.UseVisualStyleBackColor = true;
+            //this.btDayGet.Click += new System.EventHandler(this.btDayGet_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(395, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "週間天気";
+            // 
+            // tbDay
+            // 
+            this.tbDay.Location = new System.Drawing.Point(399, 36);
+            this.tbDay.Multiline = true;
+            this.tbDay.Name = "tbDay";
+            this.tbDay.Size = new System.Drawing.Size(389, 42);
+            this.tbDay.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pbWether);
+            this.Controls.Add(this.tbDay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbDay);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.PrefectureName);
             this.Controls.Add(this.cbPrifecture);
+            this.Controls.Add(this.btDayGet);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btWetherGet);
             this.Controls.Add(this.tbWetherInfo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pbWether)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -117,7 +161,11 @@ namespace WetherApp {
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbPrifecture;
         private System.Windows.Forms.Label PrefectureName;
-        private System.Windows.Forms.PictureBox pbWether;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbDay;
+        private System.Windows.Forms.Button btDayGet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDay;
     }
 }
 
--- chapter14/WetherApp/Form1.cs
+++ chapter14/WetherApp/Form1.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace WetherApp {
     
     public partial class Form1 : Form {
@@ -22,6 +23,7 @@ namespace WetherApp {
         }
 
         string areaCode;
+        Int32 a;
 
         private void btWetherGet_Click(object sender, EventArgs e) {
 
@@ -36,9 +38,7 @@ namespace WetherApp {
                 item = (Prefecture)cbPrifecture.SelectedItem;
 
                 areaCode = item.Code;
-                pbWether.ImageLocation = item.Image;
-                pbWether.MaximumSize = pbWether.Size;
-                pbWether.MinimumSize = pbWether.Size;
+                
 
             }
 
@@ -47,22 +47,52 @@ namespace WetherApp {
 
             var json = JsonConvert.DeserializeObject<Rootobject>(dString);
 
+            //var cString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/forecast/" + areaCode + ".json");
+
+            //var json2 = JsonConvert.DeserializeObject<Class1[]>(cString);
+
 
             tbWetherInfo.Text = json.text;
-                                      
+            //tbDay.Text = json2[0].timeSeries[0].areas[0].weathers[0];                          
+        }
+
+        public class Day {
+            private string DayCode = "";
+            private Int32 DayNumber; 
+
+            public Day(string day,Int32 Dnumber) {
+                DayCode = day;
+                DayNumber = Dnumber;
+            }
+
+            public string DayName {
+                get {
+                    return DayCode;
+                }
+            }
+
+            public Int32 dNumber {
+                get {
+                    return DayNumber;
+                }
+            }
+
+            public override string ToString() {
+                return DayCode;
+            }
         }
 
  
         public class Prefecture {
             private string wetherCode = "";
             private string PrefectureName = "";
-            private string PrefectureImage = ""; 
+             
 
 
-            public Prefecture(string code, string name, string prefimage) {
+            public Prefecture(string code, string name) {
                 wetherCode = code;
                 PrefectureName = name;
-                PrefectureImage = prefimage;
+                
             }
 
             public string Code {
@@ -77,11 +107,7 @@ namespace WetherApp {
                 }
             }
 
-            public string Image {
-                get {
-                    return PrefectureImage;
-                }
-            }
+            
 
             public override string ToString() {
                 return PrefectureName;
@@ -90,150 +116,192 @@ namespace WetherApp {
         private void Form1_Load(object sender, EventArgs e) {
             Prefecture item;
 
-            item = new Prefecture("011000", "北海道", @"C:\Users\infosys\Pictures\Saved Pictures\北海道.jpg");
+            item = new Prefecture("011000", "北海道");
             cbPrifecture.Items.Add(item);
 
-            item = new Prefecture("020000","青森", @"C:\Users\infosys\Pictures\Saved Pictures\青森.jpg");
+            item = new Prefecture("020000","青森");
             cbPrifecture.Items.Add(item);
 
-            item = new Prefecture("050000", "秋田", @"C:\Users\infosys\Pictures\Saved Pictures\秋田.jpg");
+            item = new Prefecture("050000", "秋田");
             cbPrifecture.Items.Add(item);
 
-            item = new Prefecture("030000", "岩手", @"C:\Users\infosys\Pictures\Saved Pictures\岩手.jpg");
+            item = new Prefecture("030000", "岩手");
             cbPrifecture.Items.Add(item);
 
-            item = new Prefecture("040000", "宮城", @"C:\Users\infosys\Pictures\Saved Pictures\宮城.jpg");
+            item = new Prefecture("040000", "宮城");
             cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("060000", "山形");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("060000", "山形");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("070000", "福島");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("070000", "福島");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("080000","茨城");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("080000", "茨城");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("090000", "栃木");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("090000", "栃木");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("100000", "群馬");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("100000", "群馬");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("110000", "埼玉");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("110000", "埼玉");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("130000", "東京");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("130000", "東京");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("120000", "千葉");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("120000", "千葉");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("140000", "神奈川");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("140000", "神奈川");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("200000", "長野");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("200000", "長野");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("190000", "山梨");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("190000", "山梨");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("220000", "静岡");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("220000", "静岡");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("230000", "愛知");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("230000", "愛知");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("210000", "岐阜");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("210000", "岐阜");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("240000", "三重");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("240000", "三重");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("150000", "新潟");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("150000", "新潟");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("160000", "富山");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("160000", "富山");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("170000", "石川");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("170000", "石川");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("180000", "福井");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("180000", "福井");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("250000", "滋賀");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("250000", "滋賀");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("260000", "京都");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("260000", "京都");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("270000", "大阪");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("270000", "大阪");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("280000", "兵庫");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("280000", "兵庫");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("290000", "奈良");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("290000", "奈良");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("300000", "和歌山");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("300000", "和歌山");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("330000", "岡山");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("330000", "岡山");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("340000", "広島");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("340000", "広島");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("320000", "島根");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("320000", "島根");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("310000", "鳥取");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("310000", "鳥取");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("360000", "徳島");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("360000", "徳島");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("370000", "香川");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("370000", "香川");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("380000", "愛媛");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("380000", "愛媛");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("390000", "高知");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("390000", "高知");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("350000", "山口");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("350000", "山口");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("400000", "福島");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("400000", "福島");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("440000", "大分");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("440000", "大分");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("420000", "長崎");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("420000", "長崎");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("410000", "佐賀");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("410000", "佐賀");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("430000", "熊本");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("430000", "熊本");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("450000", "宮崎");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("450000", "宮崎");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("460100", "鹿児島");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("460100", "鹿児島");
+            cbPrifecture.Items.Add(item);
 
-            //item = new Prefecture("471000", "沖縄");
-            //cbPrifecture.Items.Add(item);
+            item = new Prefecture("471000", "沖縄");
+            cbPrifecture.Items.Add(item);
 
-            
-        } 
-       
+
+
+            Day days;
+
+            days = new Day("今日",0);
+            cbDay.Items.Add(days);
+
+            days = new Day("明日",1);
+            cbDay.Items.Add(days);
+
+            days = new Day("明後日",2);
+            cbDay.Items.Add(days);
+        }
+
+        private void btDayGet_Click(object sender, EventArgs e) {
+            var wcs = new WebClient() {
+                Encoding = Encoding.UTF8
+            };
+
+            if (cbPrifecture.SelectedIndex != -1) {
+                Prefecture item;
+
+                item = (Prefecture)cbPrifecture.SelectedItem;
+
+                areaCode = item.Code;
+
+
+            }
+
+            if (cbDay.SelectedIndex != -1) {
+                Day days;
+
+                days = (Day)cbDay.SelectedItem;
+
+                a = days.dNumber;
+
+
+            }
+
+            var cString = wcs.DownloadString("https://www.jma.go.jp/bosai/forecast/data/forecast/" + areaCode + ".json");
+
+            var json2 = JsonConvert.DeserializeObject<Class1[]>(cString);
+
+            tbDay.Text = json2[a].timeSeries[a].areas[a].weathers[a];
+        }
     }
 }
 
